3bad0298a9a45409428caeea05f13ac4559e378a
mark wesly <markwesly125@gmail.com>
2026-02-20 00:40:38 +0300

    Add gunicorn for production

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask import send_from_directory
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
-
+from flask import Flask
 app = Flask(__name__)
 app.secret_key = "super-secret-key"
 
